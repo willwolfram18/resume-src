dfe0ffd6eab55b9a6b1da69ba46b380e9fbf6cdf
Billy Wolfington <wolfington98@gmail.com>
2018-11-11 22:48:03 -0800

    React loaded

---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+import * as React from  "react";
+
+export class App extends React.Component {
+    render() {
+        return (
+            <h2>
+                Hello, world!
+            </h2>
+        )
+    }
+}
---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import { App } from "@app/components/App";
+
+ReactDOM.render(
+    <App />,
+    document.getElementById("App")
+);
